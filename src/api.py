cc152b621e063a542d9ed327ae758746663bdabe
Adinath-S-Pawar <21aspawar16055@gmail.com>
2026-09-17 10:48:45 +0530

    handle production base url and cors

--- src/api.py
+++ src/api.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Fraud Detection API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
